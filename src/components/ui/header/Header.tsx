d9bc8fa0f0cb28567c49800a6b789cb3497d9a43
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-07-18 16:44:14 -0300

    Header de Ejemplo

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,13 +1,42 @@
 import './Header.css';
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useState } from 'react';
 
+// import logo from "../../../assets/icons/logo_smt_negro.png";
 
 export const Header = () => {
 
+    
+
+    const [changeIcon, setChangeIcon] = useState(true);
+
+    const onHandleChangeIcon = () => {
+        setChangeIcon(!changeIcon);
+    };
 
   return (
     <div className='Header__container'>
-        <div className="Header__container-icon"> LOGO SMARTRAINING</div>
-        <div className="Header__container-nav">
+        <div className="Header__container-icon">
+            <img 
+                // src={logo}
+                alt="LogoSMT" 
+                className='container__icon-img'
+            />
+        </div>
+        <div className="container__box-iconMobile" onClick={onHandleChangeIcon}>
+            {changeIcon ? (
+                <FaBars className='box-iconMobile '/>
+            ) : (
+                <FaTimes className='box-iconMobile '/>
+            )}
+        </div>
+        <div onClick={onHandleChangeIcon}
+            className={
+                changeIcon
+                  ? "Header__container-nav moveToLeft"
+                  : "Header__container-nav menuClosed"
+              }
+        >
             <ul className='container__nav-ul'>
                 <li className='nav__ul-li'><a href="#" className='nav__ul-a'>HOME</a></li>
                 <li className='nav__ul-li'><a href="#" className='nav__ul-a'>CUSTOMERS</a></li>
