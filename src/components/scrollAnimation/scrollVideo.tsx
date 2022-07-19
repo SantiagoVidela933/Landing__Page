9b3a4f02d36529e86d71a7ed491a996f320e309e
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-07-19 09:58:13 -0300

    Uniendo scrollVideo

---
+++ src/components/scrollAnimation/scrollVideo.tsx
@@ -0,0 +1,14 @@
+
+export const scrollVideo = () => {
+  return (
+    <div>
+        <div className="ScrollVideo__intro">
+            <h1>Prototipo Scrollbar Animation</h1>
+            <video src="../../assets/videos/"></video>
+        </div>
+        <section>
+            <h1>Ideologik</h1>
+        </section>
+    </div>
+  )
+}
