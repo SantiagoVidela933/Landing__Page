80e12044137a11bb54b82e37292bea11d0dcb5a5
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-07-20 10:45:13 -0300

    Armado de Home y content

--- src/components/sections/contentSection/ContentSection.tsx
+++ src/components/sections/contentSection/ContentSection.tsx
@@ -6,6 +6,7 @@ export const ContentSection = () => {
         <div className='ContentSection__container-info'>
             <h2 className='container__info-h2'>Fire up your performance with powerful analytics.</h2>
             <p className='container__info-p'>Objective assessment and coaching to guide training and measure proficiency.</p>
+            {/* <a href="#" className='container__info-a'>Learn more about assessment and analytics</a> */}
             <button className='container__info-btn'>Learn more about assessment and analytics</button>
         </div>
         <div className='ContentSection__container-carrousel'></div>
