9daa90f567dd555cefb373f541f390d987203e2e
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-07-19 22:54:12 -0300

    Acomodando Fuentes

--- src/components/sections/mainSection/HomeSection.tsx
+++ src/components/sections/mainSection/HomeSection.tsx
@@ -9,7 +9,8 @@ export const HomeSection = () => {
             <div className="container__box-info">
                 <h2 className='box__info-h2'>Inspire healthcare professionals at scale.</h2>
                 <p className='box__info-p'>Virtual training experiences that accelerate learning and drive adoption.</p>
-                <button className='box__info-btn'>Learn more about our platform</button>
+                {/* <button className='box__info-btn'>Learn more about our platform</button> */}
+                <a href="#" className='box__info-btn'>Learn more about our platform</a>
             </div>
             <div className="container__box-sample">
                 <div className='box__sample-div'></div> 
