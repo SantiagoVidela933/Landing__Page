7bcefc60a8869129d5012b864c60b5495be0212c
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-07-20 15:13:48 -0300

    ContentSection Styles

--- src/components/sections/contentSection/ContentSection.tsx
+++ src/components/sections/contentSection/ContentSection.tsx
@@ -4,7 +4,7 @@ export const ContentSection = () => {
   return (
     <div className='ContentSection__container'>
       <div className='ContentSection__container-box'>
-        <div className='container__box-info'>
+        <div className='ContentSection__box-info'>
             <h2 className='info-h2'>Fire up your performance with powerful analytics.</h2>
             <p className='info-p'>Objective assessment and coaching to guide training and measure proficiency.</p>
             <button className='info-btn'>Learn more about assessment and analytics</button>
