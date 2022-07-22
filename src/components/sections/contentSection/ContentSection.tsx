737208cb23b8b7f7724d0b7add90d947ea2dc0e4
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-07-22 17:10:05 -0300

    Cambiando nombres de varibales

--- src/components/sections/contentSection/ContentSection.tsx
+++ src/components/sections/contentSection/ContentSection.tsx
@@ -4,14 +4,14 @@ export const ContentSection = () => {
   return (
     <div className='ContentSection__container'>
       <div className='ContentSection__container-box'>
-        <div className='ContentSection__box-info'>
-            <h2 className='info-h2'>Fire up your performance with powerful analytics.</h2>
-            <p className='info-p'>Objective assessment and coaching to guide training and measure proficiency.</p>
-            <button className='info-btn'>Learn more about assessment and analytics</button>
+        <div className='ContentSection__container-box-info'>
+            <h2 className='ContentSection__container-box-info-h2'>Fire up your performance with powerful analytics.</h2>
+            <p className='ContentSection__container-box-info-p'>Objective assessment and coaching to guide training and measure proficiency.</p>
+            <button className='ContentSection__container-box-info-btn'>Learn more about assessment and analytics</button>
         </div>
-        <div className='container__box-div'>
-            <div className='box-div example-box'></div>
-            <div className='box-div'></div>
+        <div className='ContentSection__container-box-animation'>
+            <div className='ContentSection__container-box-animation-video'></div>
+            <div className='ContentSection__container-box-animation-video'></div>
         </div>
       </div>
     </div>
--- src/components/sections/mainSection/HomeSection.tsx
+++ src/components/sections/mainSection/HomeSection.tsx
@@ -5,14 +5,13 @@ export const HomeSection = () => {
   return (
     <div className="HomeSection__container">
         <div className="HomeSection__container-box">
-            <div className="container__box-info">
-                <h2 className='box__info-h2'>Inspire healthcare professionals at scale.</h2>
-                <p className='box__info-p'>Virtual training experiences that accelerate learning and drive adoption.</p>
-                {/* <button className='box__info-btn'></button> */}
-                <MDBBtn className='box__info-btn'>Learn more about our platform</MDBBtn>
+            <div className="HomeSection__container-box-info">
+                <h2 className='HomeSection__container-box-info-h2'>Inspire healthcare professionals at scale.</h2>
+                <p className='HomeSection__container-box-info-p'>Virtual training experiences that accelerate learning and drive adoption.</p>
+                <MDBBtn className='HomeSection__container-box-info-btn'>Learn more about our platform</MDBBtn>
             </div>
-            <div className="container__box-sample">
-                <div className='box__sample-div'></div> 
+            <div className="HomeSection__container-box-animation">
+                <div className='HomeSection__container-box-animation-video'></div> 
             </div>
         </div>
     </div>
