48429edebbb07d8150b4ac01012995ac6180036b
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-07-22 11:47:30 -0300

    Arreglando Malas Practicas

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,5 +1,4 @@
 import { Header } from './components/ui/header/Header'
-import { VideoSection } from './components/sections/videoSection/VideoSection'
 import { HomeSection } from './components/sections/mainSection/HomeSection'
 import { ContentSection } from './components/sections/contentSection/ContentSection'
 import './styles/LandingPage.css'
@@ -8,7 +7,6 @@ export const LandingPage = () => {
   return (
     <div className='LandingPage__container'>
       <Header/>
-      <VideoSection/>
       <HomeSection/>
       <ContentSection/>
     </div>
--- src/components/sections/videoSection/VideoSection.tsx
+++
@@ -1,9 +0,0 @@
-import './VideoSection.css';
-
-export const VideoSection = () => {
-  return (
-    <div className='VideoSection__container'>
-        <div className='VideoSection__container-box'></div>
-    </div>
-  )
-}
