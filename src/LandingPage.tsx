b572f2b866456286a53cfe3bed04caca01d4c448
SantiagoVidela933 <santi.videla.2@gmail.com>
2022-07-26 15:29:52 -0300

    Agregando Footer de MDB

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,19 +1,24 @@
 import { Header } from './components/ui/header/Header';
+import { HeaderSection } from './components/sections/HeaderSection/HeaderSection';
 import { HomeSection } from './components/sections/HomeSection/HomeSection';
 import { PhoneSection } from './components/sections/PhoneSection/PhoneSection';
 import { HeadsetSection } from './components/sections/HeadsetSection/HeadsetSection';
 import { ComputerSection } from './components/sections/ComputerSection/ComputerSection';
+import { Footer } from './components/ui/footer/Footer';
 
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import './styles/LandingPage.css'
 
 export const LandingPage = () => {
   return (
     <div className='LandingPage__container'>
       <Header/>
+      <HeaderSection/>
       <HomeSection/>
       <PhoneSection/>
       <HeadsetSection/>
       <ComputerSection/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/sections/HeaderSection/HeaderSection.tsx
@@ -0,0 +1,13 @@
+import './HeaderSection.css';
+
+export const HeaderSection = () => {
+  return (
+    <div className='HeaderSection__container'>
+        <div className="HeaderSection__container-box">
+            <h1 className="HeaderSection__container-box-h1">Operate at a higher level.</h1>
+            <p className="HeaderSection__container-box-p    ">The leading virtual reality surgical training and assessment platform.</p>
+            <button className='HeaderSection__container-box-btn'>REQUEST A DEMO</button>
+        </div>
+    </div>
+  )
+}
